ac2a1f6b6373b68820887d63e7e5d7c8b72ba5e1
AyShanYa <1911077033@qq.com>
2024-11-09 20:48:03 +0800

    对书架界面加入滚动，并简单设计了设置页面

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/testreader/databinding/FragmentHomeBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/testreader/databinding/FragmentHomeBinding.java
@@ -5,11 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
 import androidx.viewbinding.ViewBindings;
 import com.example.testreader.R;
@@ -19,29 +17,19 @@ import java.lang.String;
 
 public final class FragmentHomeBinding implements ViewBinding {
   @NonNull
-  private final ConstraintLayout rootView;
-
-  @NonNull
-  public final ImageView backgroundImage;
+  private final LinearLayout rootView;
 
   @NonNull
   public final GridView bookshelfGridview;
 
-  @NonNull
-  public final TextView textHome;
-
-  private FragmentHomeBinding(@NonNull ConstraintLayout rootView,
-      @NonNull ImageView backgroundImage, @NonNull GridView bookshelfGridview,
-      @NonNull TextView textHome) {
+  private FragmentHomeBinding(@NonNull LinearLayout rootView, @NonNull GridView bookshelfGridview) {
     this.rootView = rootView;
-    this.backgroundImage = backgroundImage;
     this.bookshelfGridview = bookshelfGridview;
-    this.textHome = textHome;
   }
 
   @Override
   @NonNull
-  public ConstraintLayout getRoot() {
+  public LinearLayout getRoot() {
     return rootView;
   }
 
@@ -66,26 +54,13 @@ public final class FragmentHomeBinding implements ViewBinding {
     // This is done to optimize the compiled bytecode for size and performance.
     int id;
     missingId: {
-      id = R.id.background_image;
-      ImageView backgroundImage = ViewBindings.findChildViewById(rootView, id);
-      if (backgroundImage == null) {
-        break missingId;
-      }
-
       id = R.id.bookshelf_gridview;
       GridView bookshelfGridview = ViewBindings.findChildViewById(rootView, id);
       if (bookshelfGridview == null) {
         break missingId;
       }
 
-      id = R.id.text_home;
-      TextView textHome = ViewBindings.findChildViewById(rootView, id);
-      if (textHome == null) {
-        break missingId;
-      }
-
-      return new FragmentHomeBinding((ConstraintLayout) rootView, backgroundImage,
-          bookshelfGridview, textHome);
+      return new FragmentHomeBinding((LinearLayout) rootView, bookshelfGridview);
     }
     String missingId = rootView.getResources().getResourceName(id);
     throw new NullPointerException("Missing required view with ID: ".concat(missingId));
